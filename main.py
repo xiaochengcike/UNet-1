deda0974978606dd4c1a130fa9e2b7d553079537
wulalago <47970928+wulalago@users.noreply.github.com>
2019-03-01 17:03:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from torch.autograd import Variable
 
 from utils import AvgMeter, dice_coeff, check_dir
 from dataop import MySet, create_list
-from Network import SegNet, FCN, DAF3D, hNet
 from init import InitParser
+import UNet
 
 
 def test_epoch(net, loader):
+    # we transfer the mode of network to test
     net.eval()
     test_dice_meter = AvgMeter()
     for batch_idx, (data, label) in enumerate(loader):
@@ -28,7 +29,9 @@ def test_epoch(net, loader):
 
 
 def train_epoch(net, loader, optimizer, cost):
+    # we transfer the mode of network to train
     net.train()
+    
     batch_loss = AvgMeter()
     for batch_idx, (data, label) in enumerate(loader):
         data = Variable(data.cuda())
@@ -57,29 +60,33 @@ def main(args):
 
     torch.cuda.set_device(args.gpu_id)
     train_list, test_list = create_list(args.data_path, ratio=args.train_ratio)
-
+    
+    # define the dataset and loader
     train_set = MySet(train_list)
     train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
     test_set = MySet(test_list)
     test_loader = DataLoader(test_set, batch_size=1, shuffle=False)
-
-    net = hNet.h_Net().cuda()
+    
+    # define the network and load the init weight
+    net = UNet.UNet().cuda()
     if args.is_load:
         net.load_state_dict(torch.load(args.load_path))
-
+    
+    # define the optimizer of the training process
     optimizer = torch.optim.Adam(
         net.parameters(),
         lr=args.lr,
     )
-
+    
+    # define the loss function
     cost = torch.nn.BCELoss()
     best_dice = 0.
     for epoch in range(args.init_epoch, args.init_epoch+args.num_epoch):
         start_time = time.time()
-
+        # train one epoch
         epoch_loss = train_epoch(net, train_loader, optimizer, cost)
         epoch_time = time.time() - start_time
-
+        # eval in test data after one epoch training
         epoch_dice = test_epoch(net, test_loader)
 
         info_line = "Epoch {} || Loss: {:.4f} | Time: {:.2f} | Test Dice: {:.4f} ".format(
@@ -87,7 +94,8 @@ def main(args):
         )
         print(info_line)
         open(os.path.join(log_path, 'train_log.txt'), 'a').write(info_line+'\n')
-
+        
+        # save the checkpoint
         torch.save(net.state_dict(), os.path.join(ckpt_path, "Network_{}.pth.gz".format(epoch)))
         if epoch_dice > best_dice:
             best_dice = epoch_dice
